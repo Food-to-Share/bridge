b0ae64fe861de3ba451438bcbd262e253f430d27
kevinDomingues <77273627+kevinDomingues@users.noreply.github.com>
2022-04-21 18:42:07 +0100

    initial-config

---
+++ config/registration.go
@@ -0,0 +1,49 @@
+package config
+
+import (
+	"regexp"
+
+	"maunium.net/go/mautrix/appservice"
+)
+
+func (config *Config) NewRegistration() (*appservice.Registration, error) {
+	registration := appservice.CreateRegistration()
+
+	err := config.copyToRegistration(registration)
+	if err != nil {
+		return nil, err
+	}
+
+	config.AppService.ASToken = registration.AppToken
+	config.AppService.HSToken = registration.ServerToken
+
+	return registration, nil
+}
+
+func (config *Config) GetRegistration() (*appservice.Registration, error) {
+	registration := appservice.CreateRegistration()
+
+	err := config.copyToRegistration(registration)
+	if err != nil {
+		return nil, err
+	}
+
+	registration.AppToken = config.AppService.ASToken
+	registration.ServerToken = config.AppService.HSToken
+	return registration, nil
+}
+
+func (config *Config) copyToRegistration(registration *appservice.Registration) error {
+	registration.ID = config.AppService.ID
+	registration.URL = config.AppService.Address
+	falseVal := false
+	registration.RateLimited = &falseVal
+	registration.SenderLocalpart = config.AppService.Bot.Username
+
+	userIDRegex, err := regexp.Compile(config.Bridge.FormatUsername("[0-9]+", "[0-9]+"))
+	if err != nil {
+		return err
+	}
+	registration.Namespaces.RegisterUserIDs(userIDRegex, true)
+	return nil
+}
