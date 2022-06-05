6f6ad96a17d322822e147a24efaa134783b7f9d5
kevinDomingues <77273627+kevinDomingues@users.noreply.github.com>
2022-06-05 18:36:32 +0100

    lookup done

--- provisioning.go
+++ provisioning.go
@@ -11,6 +11,8 @@ import (
 	"maunium.net/go/mautrix/id"
 )
 
+var IdentityServerURL = "http://localhost:8090"
+
 type ProvisioningAPI struct {
 	bridge *Bridge
 	log    log.Logger
@@ -23,8 +25,8 @@ func (prov *ProvisioningAPI) Init() {
 
 	r := prov.bridge.AS.Router.PathPrefix(prov.bridge.Config.AppService.Provisioning.Prefix).Subrouter()
 	r.HandleFunc("/v1/startUser", prov.StartUser).Methods(http.MethodPost)
-	r.HandleFunc("/v1/syncNumber", prov.SyncNumber).Methods(http.MethodPost)
-	r.HandleFunc("/v1/syncEmail", prov.SyncEmail).Methods(http.MethodPost)
+	r.HandleFunc("/v1/syncNiss", prov.SyncNiss).Methods(http.MethodPost)
+	r.HandleFunc("/v1/lookup", prov.Lookup).Methods(http.MethodPost)
 }
 
 type Error struct {
@@ -48,9 +50,9 @@ type NumberToSync struct {
 	Number string `json:"number"`
 }
 
-type EmailToSync struct {
-	Jid   string `json:"jid"`
-	Email string `json:"email"`
+type NissToSync struct {
+	Jid  string `json:"jid"`
+	Niss string `json:"niss"`
 }
 
 type RequestTokenNumber struct {
@@ -60,9 +62,9 @@ type RequestTokenNumber struct {
 	Send_attempt  int    `json:"send_attempt"`
 }
 
-type RequestTokenEmail struct {
+type RequestTokenNiss struct {
 	Client_Secret string `json:"client_secret"`
-	Email         string `json:"email"`
+	Niss          string `json:"niss"`
 	Send_attempt  int    `json:"send_attempt"`
 }
 
@@ -77,8 +79,18 @@ type RequestTokenNumberResp struct {
 	Msisdn string `json:"msisdn"`
 }
 
-type RequestTokenEmailResp struct {
+type RequestTokenNissResp struct {
 	Sid string `json:"sid"`
+}
+
+type LookupReq struct {
+	Niss string `json:"niss"`
+}
+
+type LookupResp struct {
+	Medium  string `json:"medium"`
+	Address string `json:"address"`
+	MXID    string `json:"mxid"`
 }
 
 type OtherUserInfo struct {
@@ -118,20 +130,21 @@ func (prov *ProvisioningAPI) StartUser(w http.ResponseWriter, r *http.Request) {
 	})
 }
 
-func (prov *ProvisioningAPI) SyncNumber(w http.ResponseWriter, r *http.Request) {
+func (prov *ProvisioningAPI) SyncNiss(w http.ResponseWriter, r *http.Request) {
 	// user := prov.bridge.GetUserByMXID(id.UserID("@kevindom:localhost"))
-	jid, number := prov.resolveIdentifierNumber(w, r)
+	jid, niss := prov.resolveIdentifierNiss(w, r)
 
 	fmt.Println("JID: " + jid)
-	fmt.Println("Number: " + number)
-	// puppet := prov.bridge.GetPuppetByJID(jid)
+	fmt.Println("Niss: " + niss)
+	puppet := prov.bridge.GetPuppetByJID(jid)
 
-	httpposturl := "http://localhost:8090/_matrix/identity/api/v1/validate/msisdn/requestToken"
+	fmt.Println("MXID: " + puppet.MXID)
 
-	requestToken := RequestTokenNumber{
+	httpposturl := IdentityServerURL + "/_matrix/identity/api/v1/validate/niss/requestToken"
+
+	requestToken := RequestTokenNiss{
 		Client_Secret: "uij4hri2n4h42jn34k2n4nmwenjhjhnrj3n4j1b4",
-		Country:       "pt",
-		Phone_number:  number,
+		Niss:          niss,
 		Send_attempt:  1,
 	}
 
@@ -154,39 +167,28 @@ func (prov *ProvisioningAPI) SyncNumber(w http.ResponseWriter, r *http.Request) 
 			//Failed to read response.
 			panic(err)
 		}
-		var rtr RequestTokenNumberResp
+		var rtr RequestTokenNissResp
 
 		err = json.Unmarshal(respbody, &rtr)
+
+		// newRequest := BindToken{
+		// 	Client_Secret: "uij4hri2n4h42jn34k2n4nmwenjhjhnrj3n4j1b4",
+		// 	Mxid:          string(puppet.MXID),
+		// 	Sid:           rtr.Sid,
+		// }
+
+		// prov.bind(w, r, newRequest)
 		status := http.StatusOK
 		jsonResponse(w, status, rtr)
 	}
 }
 
-func (prov *ProvisioningAPI) SyncEmail(w http.ResponseWriter, r *http.Request) {
-	// user := prov.bridge.GetUserByMXID(id.UserID("@kevindom:localhost"))
-	jid, email := prov.resolveIdentifierEmail(w, r)
+func (prov *ProvisioningAPI) Lookup(w http.ResponseWriter, r *http.Request) {
+	niss := prov.resolveIdentifierNissToLookup(w, r)
 
-	fmt.Println("JID: " + jid)
-	fmt.Println("Email: " + email)
-	puppet := prov.bridge.GetPuppetByJID(jid)
+	httpposturl := IdentityServerURL + "/_matrix/identity/api/v1/lookup?medium=niss&address=" + niss
 
-	fmt.Println("MXID: " + puppet.MXID)
-
-	httpposturl := "http://localhost:8090/_matrix/identity/api/v1/validate/email/requestToken"
-
-	requestToken := RequestTokenEmail{
-		Client_Secret: "uij4hri2n4h42jn34k2n4nmwenjhjhnrj3n4j1b4",
-		Email:         email,
-		Send_attempt:  1,
-	}
-
-	data, err := json.Marshal(requestToken)
-	if err != nil {
-		log.Fatal(err)
-	}
-	reader := bytes.NewBuffer(data)
-
-	response, errorPost := http.Post(httpposturl, "application/json", reader)
+	response, errorPost := http.Get(httpposturl)
 	if errorPost != nil {
 		panic(errorPost)
 	}
@@ -199,25 +201,25 @@ func (prov *ProvisioningAPI) SyncEmail(w http.ResponseWriter, r *http.Request) {
 			//Failed to read response.
 			panic(err)
 		}
-		var rtr RequestTokenEmailResp
+		var lr LookupResp
 
-		err = json.Unmarshal(respbody, &rtr)
+		err = json.Unmarshal(respbody, &lr)
 
-		newRequest := BindToken{
-			Client_Secret: "uij4hri2n4h42jn34k2n4nmwenjhjhnrj3n4j1b4",
-			Mxid:          string(puppet.MXID),
-			Sid:           rtr.Sid,
+		if lr.MXID == "" {
+			lr.MXID = "None"
+			lr.Medium = "niss"
+			lr.Address = "Not in the system"
 		}
 
-		prov.bind(w, r, newRequest)
-		// status := http.StatusOK
-		// jsonResponse(w, status, rtr)
+		status := http.StatusOK
+		jsonResponse(w, status, lr)
 	}
 }
 
+/*  This was only necessary with mxisd server, with sydent the bind is done automatically        */
 func (prov *ProvisioningAPI) bind(w http.ResponseWriter, r *http.Request, newRequest BindToken) {
 
-	httpposturl := "http://localhost:8090/_matrix/identity/api/v1/3pid/bind"
+	httpposturl := IdentityServerURL + "/_matrix/identity/api/v1/3pid/bind"
 
 	data, err := json.Marshal(newRequest)
 	if err != nil {
@@ -247,7 +249,7 @@ func (prov *ProvisioningAPI) bind(w http.ResponseWriter, r *http.Request, newReq
 	}
 }
 
-func (prov *ProvisioningAPI) resolveIdentifierEmail(w http.ResponseWriter, r *http.Request) (string, string) {
+func (prov *ProvisioningAPI) resolveIdentifierNiss(w http.ResponseWriter, r *http.Request) (string, string) {
 	b, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		http.Error(w, err.Error(), 500)
@@ -255,42 +257,42 @@ func (prov *ProvisioningAPI) resolveIdentifierEmail(w http.ResponseWriter, r *ht
 	}
 
 	defer r.Body.Close()
-	var es EmailToSync
+	var es NissToSync
 
 	err = json.Unmarshal(b, &es)
 
 	if err != nil {
 		jsonResponse(w, http.StatusNotFound, Error{
-			Error:   fmt.Sprintf("Email not found"),
+			Error:   fmt.Sprintf("Niss not found"),
 			ErrCode: "Not found!",
 		})
 		return "", ""
 	}
 
-	return es.Jid, es.Email
+	return es.Jid, es.Niss
 }
 
-func (prov *ProvisioningAPI) resolveIdentifierNumber(w http.ResponseWriter, r *http.Request) (string, string) {
+func (prov *ProvisioningAPI) resolveIdentifierNissToLookup(w http.ResponseWriter, r *http.Request) string {
 	b, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		http.Error(w, err.Error(), 500)
-		return "", ""
+		return ""
 	}
 
 	defer r.Body.Close()
-	var ns NumberToSync
+	var niss LookupReq
 
-	err = json.Unmarshal(b, &ns)
+	err = json.Unmarshal(b, &niss)
 
 	if err != nil {
 		jsonResponse(w, http.StatusNotFound, Error{
-			Error:   fmt.Sprintf("Number not found"),
+			Error:   fmt.Sprintf("Niss not found"),
 			ErrCode: "Not found!",
 		})
-		return "", ""
+		return ""
 	}
 
-	return ns.Jid, ns.Number
+	return niss.Niss
 }
 
 func (prov *ProvisioningAPI) resolveIdentifier(w http.ResponseWriter, r *http.Request) (string, string) {
